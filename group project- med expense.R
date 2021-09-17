b75d7bf90780252340e5404ea386393a909334d5
guerravanessa13 <90932005+guerravanessa13@users.noreply.github.com>
2021-09-17 14:11:09 -0500

    Add files via upload
    Code for summary stats of variables for med expenses data

---
+++ group project- med expense.R
@@ -0,0 +1,28 @@
+#set working directory for med expense data
+
+#read in data
+MedExpenseData1 = read.csv("MEPS_MedExpenses_Data.csv")
+
+#View function to view data variables
+View(MedExpenseData1)
+
+#use summary to get stats on data
+summary(MedExpenseData1)
+
+#getting standard deviation
+sd(MedExpenseData1[["Total_MedExpenses_Dollars"]])
+
+sd(MedExpenseData1[["Annual_Income_Dollars"]])
+
+sd(MedExpenseData1[["Age"]])
+
+sd(MedExpenseData1[["Number_of_Doctor_Vists"]])
+
+sd(MedExpenseData1[["Number_of_ER_Vists"]])
+
+#getting categorical info
+table(MedExpenseData1[["Gender"]])
+
+table(MedExpenseData1[["Race"]])
+
+table(MedExpenseData1[["Has_Health_Insurance"]])
