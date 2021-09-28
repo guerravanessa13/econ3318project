023c9f2f9c699d674b620761b6ae30ae7733f92d
guerravanessa13 <90932005+guerravanessa13@users.noreply.github.com>
2021-09-28 16:58:10 -0500

    Add files via upload

--- group project- med expense.R
+++ group project- med expense.R
@@ -20,9 +20,20 @@ sd(MedExpenseData1[["Number_of_Doctor_Vists"]])
 
 sd(MedExpenseData1[["Number_of_ER_Vists"]])
 
+
 #getting categorical info
 table(MedExpenseData1[["Gender"]])
 
 table(MedExpenseData1[["Race"]])
 
 table(MedExpenseData1[["Has_Health_Insurance"]])
+
+table(MedExpenseData1[["Education"]])
+
+table(MedExpenseData1[["Has_Health_Insurance"]])
+
+#Estimating Simple Linear Regression
+reg_results = lm(Total_MedExpenses_Dollars ~ Number_of_Nights_in_Hospital, data = MedExpenseData1)
+
+#Examine Regression Results
+summary(reg_results)
